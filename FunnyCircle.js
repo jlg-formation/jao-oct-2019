c7800c0688809792981347d80e2269977f77c5e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-08 10:58:17 +0200

    class FunnyCircle

---
+++ FunnyCircle.js
@@ -0,0 +1,58 @@
+const SVGNS = "http://www.w3.org/2000/svg";
+
+class FunnyCircle {
+  constructor(svg) {
+    this.svg = svg;
+    this.config = {
+      numPoint: 50,
+      factor: 5,
+      showPoint: false,
+    };
+  }
+
+  configure(options) {
+    this.config.numPoint = options.numPoint;
+    this.config.factor = options.factor;
+    this.config.showPoint = options.showPoint;
+  }
+
+  draw() {
+
+    if (this.config.showPoint) {
+      this.showPoint();
+    }
+
+    const step = 360 / this.config.numPoint;
+    for (let i = 0; i < this.config.numPoint; i++) {
+      const angle1 = step * i * Math.PI / 180;
+      const angle2 = step * i * this.config.factor * Math.PI / 180;
+      const x1 = 500 + 400 * Math.cos(angle1);
+      const y1 = 500 + 400 * Math.sin(angle1);
+      const x2 = 500 + 400 * Math.cos(angle2);
+      const y2 = 500 + 400 * Math.sin(angle2);
+      const line = document.createElementNS(SVGNS, 'line');
+      line.setAttribute('x1', x1);
+      line.setAttribute('y1', y1);
+      line.setAttribute('x2', x2);
+      line.setAttribute('y2', y2);
+      this.svg.appendChild(line);
+    }
+  }
+
+  showPoint() {
+    const total = this.config.numPoint;
+    const step = 360 / total;
+    for (let i = 0; i < total; i++) {
+      const angle = step * i * Math.PI / 180;
+      const cx = 500 + 400 * Math.cos(angle);
+      const cy = 500 + 400 * Math.sin(angle);
+      const circle = document.createElementNS(SVGNS, 'circle');
+      circle.setAttribute('cx', cx);
+      circle.setAttribute('cy', cy);
+      circle.setAttribute('r', 10);
+      circle.setAttribute('class', 'tick');
+      this.svg.appendChild(circle);
+    }
+  }
+
+}
--- main.js
+++ main.js
@@ -1,34 +1,20 @@
 console.log('start');
 
-const svgns = "http://www.w3.org/2000/svg";
+//
+const elt = document.querySelector('svg.s1');
+const funnyCircle = new FunnyCircle(elt);
+funnyCircle.configure({
+  numPoint: 10,
+  factor: 2,
+  showPoint: true,
+});
+funnyCircle.draw();
 
-const total = 10;
-const svg = document.querySelector('svg');
-const step = 360 / total;
-for (let i = 0; i < total; i++) {
-  const angle = step * i * Math.PI / 180;
-  const cx = 500 + 400 * Math.cos(angle);
-  const cy = 500 + 400 * Math.sin(angle);
-  const circle = document.createElementNS(svgns, 'circle');
-  circle.setAttribute('cx', cx);
-  circle.setAttribute('cy', cy);
-  circle.setAttribute('r', 10);
-  circle.setAttribute('class', 'tick');
-  svg.appendChild(circle);
-}
-
-const coef = 2;
-for (let i = 0; i < total; i++) {
-  const angle1 = step * i * Math.PI / 180;
-  const angle2 = step * i * coef * Math.PI / 180;
-  const x1 = 500 + 400 * Math.cos(angle1);
-  const y1 = 500 + 400 * Math.sin(angle1);
-  const x2 = 500 + 400 * Math.cos(angle2);
-  const y2 = 500 + 400 * Math.sin(angle2);
-  const line = document.createElementNS(svgns, 'line');
-  line.setAttribute('x1', x1);
-  line.setAttribute('y1', y1);
-  line.setAttribute('x2', x2);
-  line.setAttribute('y2', y2);
-  svg.appendChild(line);
-}
+const elt2 = document.querySelector('svg.s2');
+const funnyCircle2 = new FunnyCircle(elt2);
+funnyCircle2.configure({
+  numPoint: 100,
+  factor: 20,
+  showPoint: false,
+});
+funnyCircle2.draw();
